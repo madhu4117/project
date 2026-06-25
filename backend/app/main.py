0f62fcf3731f557eab5b9cb3e043aafa9039dbff
emailmadhusudhanan4117@gmail.com <107028859+madhu4117@users.noreply.github.com>
2026-06-25 15:51:49 +0530

    updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -66,18 +66,25 @@ finally:
 app = FastAPI()
 
 
-# CORS
+# # CORS
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[
+#         "http://localhost:3000",
+#         "http://127.0.0.1:3000"
+#     ],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"]
+# )
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
-
 
 # Register routes
 app.include_router(auth_router)
